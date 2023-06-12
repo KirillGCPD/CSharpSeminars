3f63677b5d0b67b0ad9f06ae7a236e835cceb431
Kirill Nechaev <akadelpher@gmail.com>
2023-06-13 04:54:00 +0800

    task4 done

---
+++ Seminar_1/task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число А = ");
+var numberA=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B = ");
+var numberB=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C = ");
+var numberC=Convert.ToInt32(Console.ReadLine());
+var max=numberA;
+if (numberB>max)
+{  
+    max=numberB;
+}
+if (numberC>max)
+{  
+    max=numberC;
+}
+System.Console.WriteLine("Максимальное число из списка: "+max);
