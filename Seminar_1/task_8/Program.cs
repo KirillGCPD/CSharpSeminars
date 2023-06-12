982552980c47ca2bfa9f4c0ec60aa680015cab7a
Kirill Nechaev <akadelpher@gmail.com>
2023-06-13 05:07:33 +0800

    done task 8

---
+++ Seminar_1/task_8/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число = ");
+var number=Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= number; i+=2)
+{
+    if (i!=2)
+    {
+        System.Console.Write(", ");
+    }
+    System.Console.Write(i);
+}
+if (number<2)
+{
+    System.Console.WriteLine("N меньше двух, а значит вывода нет");
+}
