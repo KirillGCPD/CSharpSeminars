a0a1dc4d4d6b1649cf32f32dcf25985d01280865
Kirill Nechaev <akadelpher@gmail.com>
2023-07-01 00:00:11 +0700

    task 36

---
+++ seminar_5/task_36/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+//Генерировать массив случайных чисел заданой длины
+int [] GenerateRandomIntArray(int lenght)
+{
+    var array = new int[lenght];
+    Random randomizer=new Random();//генератор случайных чисел
+    for (int i=0;i<lenght;i++)
+    {
+        array[i]=randomizer.Next(-100,101); // 100-999
+    }
+    return array;
+}
+//Сумма элементов на четных индексах
+int SumOfElementsOnEvenPositions(int[] intArray)
+{
+    int lenght = intArray.Length; //на всякий случай сохраню длину массива
+    int result=0;
+    for (int i=0;i<lenght;i++)
+    {
+        if (i%2==1)
+        {
+            result+=intArray[i];
+        }
+    }
+    return result;
+}
+//Вывод на экран
+void PrintArray(int[] array)
+{
+    Console.WriteLine("["+string.Join(", ",array)+"]");
+}
+//+++++ОСНОВНОЙ КОД+++++
+var array= GenerateRandomIntArray(5);
+Console.Write("Сумма элементов стоящих на четных позициях в массиве: ");
+PrintArray(array);
+Console.WriteLine("Равна: "+ SumOfElementsOnEvenPositions(array));
